d6215e60add649781ef96e88cac483449f315d3f
Teodor Vidakovic <teodorv22@gmail.com>
2024-10-09 20:47:07 +0200

    refact: remove println in tree utils and add method description

--- jetbrains-task/src/main/scala/main_task/TreeUtils.scala
+++ jetbrains-task/src/main/scala/main_task/TreeUtils.scala
@@ -1,8 +1,18 @@
 package main_task
 
+/**
+ * Utility object that contains tree manipulation functions.
+ */
 object TreeUtils {
+  /**
+   * Replaces all occurrences of a specified subtree in the given tree with a replacement subtree.
+   *
+   * @param tree          The original tree where we want to perform the replacement.
+   * @param searchTree    The subtree we want to search for and replace.
+   * @param replacement   The subtree that will replace all occurrences of searchTree.
+   * @return              A new tree where all occurrences of searchTree have been replaced by replacement.
+   */
     def replace(tree: Tree, searchTree: Tree, replacement: Tree): Tree = {
-      println("tree:"+tree)
       if(tree.isEqual(searchTree)) {
         replacement
       } else {
